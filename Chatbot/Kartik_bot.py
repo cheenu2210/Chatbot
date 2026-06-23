98f3f869b77593b28890abe45a532bc0f1eb48f5
cheenu2210 <sharmakartik7291@gmail.com>
2026-06-23 13:38:29 +0530

    Update Kartik_bot.py: Rename chatbot to Kartik's Chatbot and clean up branding

--- Chatbot/Kartik_bot.py
+++ Chatbot/Kartik_bot.py
@@ -1,15 +1,12 @@
 import streamlit as st
-st.set_page_config(page_title="ThinkRook Chatbot", page_icon="🚀", layout = "centered")
+st.set_page_config(page_title="Kartik's Chatbot", page_icon="🚀", layout = "centered")
 st.logo("https://dme2wmiz2suov.cloudfront.net/Institution(8663)/Logo/4216689-ThinkRook_Logo.png")
 
-st.title("🤖 ThinkRook Chatbot")
+st.title("🤖 Kartik Chatbot")
 st.subheader("Your smart assistant for fast and reliable answers.")
 
 st.markdown(
     ":violet-badge[:material/star: Favorite]"
 )
-st.markdown("Welcome to **ThinkRook**, a simple chatbot interface designed to help you find answers quickly and efficiently.")
+st.markdown("Welcome to **Kartik**, a simple chatbot interface designed to help you find answers quickly and efficiently.")
 
-st.divider()
-
-st.markdown("_Powered by Thinknyx Technologies LLP_")
